a7fa61c0a89a3148c9f88931482d6cf47b7da2b9
Kshitij Kumar <kshitij2212@gmail.com>
2021-05-28 16:12:50 +0530

    task fragment layout done

--- app/src/main/java/com/example/atomichabits/ui/task/TaskFragment.kt
+++ app/src/main/java/com/example/atomichabits/ui/task/TaskFragment.kt
@@ -1,6 +1,28 @@
 package com.example.atomichabits.ui.task
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.atomichabits.databinding.FragmentTaskBinding
 
 class TaskFragment : Fragment() {
+
+    private var _binding: FragmentTaskBinding? = null
+    private val binding: FragmentTaskBinding get() =  _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentTaskBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
